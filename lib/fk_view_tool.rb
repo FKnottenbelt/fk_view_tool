00a9573151cf2d9d43e6b86f4de7351c2fd06ce9
Femke Knottenbelt <femknotgithub@gmail.com>
2021-09-28 09:31:35 +0200

    Build version 0.1.2 after cleaning up files

--- lib/fk_view_tool.rb
+++ lib/fk_view_tool.rb
@@ -2,8 +2,3 @@
 
 require_relative "fk_view_tool/version"
 require_relative "fk_view_tool/renderer"
-
-# module FkViewTool
-#   class Error < StandardError; end
-#   # Your code goes here...
-# end
--- lib/fk_view_tool/version.rb
+++ lib/fk_view_tool/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module FkViewTool
-  VERSION = "0.1.1"
+  VERSION = "0.1.2"
 end
